6e3dfda0dda2e542b480172fa3af6e1c874b17b8
Yago <josevicente.yago@um.es>
2021-12-02 03:19:51 +0100

    ranger & modulation in R folder

---
+++ R/linReg.R
@@ -0,0 +1,60 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+
+if (F){
+#----NOT RUN
+  library(ISLR)
+  library(MASS)
+  attach(Boston)
+
+  # multiple linear reg
+   (lm.fit = lm(medv ~ lstat + age, data = Boston))
+   saveRDS(lm.boston, file = "./inst/extdata/lm-boston.RDS")
+}
+
+
+
+#' Construir un JSON
+#'
+#' @param df A dataframe.
+#out2JSON <- function(df){
+#  jsonlite::toJSON(df)
+#}
+
+
+#' Devuelve el resultado de una regresion lineal simple ajustada al dataset "BOSTON" del paquete MASS ()
+#'
+#' @param lstat ....
+#' @param agre ....
+getPred.lm.boston <- function(lstat, age){
+
+  lm.fit <- loadModel(model.name = "lm-boston.RDS")
+  out <- predict(lm.m.fit,
+                 data.frame("lstat" = lstat,
+                            "age"   = age),
+                             interval = "confidence")
+  return(out)
+}
+
+
+if (F) {
+  # dummyML::getPred.lm.s(lstat = c(5,10,15))
+  dummyML::getPred.lm.boston(lstat = c(5,10,15), age = c(80, 90, 100))
+}
+
+
+
+
---
+++ R/utils.R
@@ -0,0 +1,4 @@
+loadModel <- function(model.name) {
+  f = system.file("extdata", model.name, package = "dummyML")
+  return(readRDS(f))
+}
