7c170d31fd34fbaf63933dd76905fb91d7663984
vyago <josevicente.yago@um.es>
2021-12-03 14:02:17 +0100

    updated filenames

--- linReg.R
+++ linReg.R
@@ -1,8 +1,8 @@
- 
+
 library(ISLR)
 library(MASS)
 attach(Boston)
 
 # multiple linear reg
 (lm.fit = lm(medv ~ lstat + age, data = Boston))
-saveRDS(lm.boston, file = ".lm-boston.RDS")
+saveRDS(lm.fit, file = "./lm-boston.RDS")
